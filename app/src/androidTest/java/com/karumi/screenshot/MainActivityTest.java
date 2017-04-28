bf65f17e1118da049f01af9b260cda436c22fb90
Luis Ibanez <libanez@Luiss-MacBook-Pro.local>
2017-04-28 16:13:00 +0200

    Added 3 tests and record new screenshots

--- app/src/androidTest/java/com/karumi/screenshot/MainActivityTest.java
+++ app/src/androidTest/java/com/karumi/screenshot/MainActivityTest.java
@@ -61,6 +61,30 @@ public class MainActivityTest extends ScreenshotTest {
     compareScreenshot(activity);
   }
 
+  @Test public void showsSuperheroIfThereIsOneSuperHero() {
+    givenThereAreSomeSuperHeroes(1, false);
+
+    Activity activity = startActivity();
+
+    compareScreenshot(activity);
+  }
+
+  @Test public void showsTwoSuperheroesIfThereAreTwoSuperHeroes() {
+    givenThereAreSomeSuperHeroes(2, false);
+
+    Activity activity = startActivity();
+
+    compareScreenshot(activity);
+  }
+
+  @Test public void showsTenSuperheroesIfThereAreTenSuperHeroes() {
+    givenThereAreSomeSuperHeroes(10, false);
+
+    Activity activity = startActivity();
+
+    compareScreenshot(activity);
+  }
+
   private List<SuperHero> givenThereAreSomeSuperHeroes(int numberOfSuperHeroes, boolean avengers) {
     List<SuperHero> superHeroes = new LinkedList<>();
     for (int i = 0; i < numberOfSuperHeroes; i++) {
